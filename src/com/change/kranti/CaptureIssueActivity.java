6fa44bcea89c6beb824aa8d8cce78761c588d236
Nikitha <nikita9.iyer@gmail.com>
2014-02-19 12:54:55 +0530

    day 2 part 1 : Using Implicit intents.Open camera - take picture - show on activity.

--- src/com/change/kranti/CaptureIssueActivity.java
+++ src/com/change/kranti/CaptureIssueActivity.java
@@ -2,13 +2,20 @@ package com.change.kranti;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import repository.IssueRepository;
+
+
 
 public class CaptureIssueActivity extends Activity {
     private IssueRepository issueRepository;
+    private Uri fileUri;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,5 +34,20 @@ public class CaptureIssueActivity extends Activity {
         issueRepository.createIssue(issueTitle,issueDescription);
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
+    }
+
+    public void callCamera(View view) {
+      Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      startActivityForResult(cameraIntent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ImageView imageView = (ImageView) findViewById(R.id.image);
+            imageView.setImageBitmap(photo);
+        }
     }
 }
